09b01efded931ca98d5cba7608eb2275346df9dc
muk863 <muk863@rambler.ru>
2021-11-28 15:10:33 +0300

    Initial Commit

---
+++ VKClientSUI/VKClientSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKClientSUIApp.swift
+//  VKClientSUI
+//
+//  Created by username on 28.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct VKClientSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
